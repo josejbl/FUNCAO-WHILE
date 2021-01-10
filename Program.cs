902a6656eef8b053ebd7e40ad5534adbbddb1b24
Your Name <you@example.com>
2021-01-10 16:22:22 -0300

    Exercicio1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FUNCAO_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Apredendo While");
+
+            Console.WriteLine("Digite um número");
+            decimal numero = decimal.Parse(Console.ReadLine());
+
+             decimal i = 0;
+
+             Console.WriteLine("------------------------------" );
+
+            //While repete enquanto a condição for verdaira
+            while(i <= numero)
+            {
+                 Console.WriteLine(i);
+                 //contador = contador + 24;// incremento
+                 // contador ++ ;
+                 i = i + (0.1m);
+
+            }//fim do While
+
+        }
+    }
+}
